5f1d91695c4c43b72656867f837fbb673d82e098
Candy Velazco <78711790+CandyVelazco100@users.noreply.github.com>
2024-08-23 12:13:09 -0700

    Funciones y argumentos

--- PythonBasics/funciones.py
+++ PythonBasics/funciones.py
@@ -1,4 +1,8 @@
-def hola():
+def hola(nom, ape = "Gomez"):
     print("Hola mundo")
+    print(f"Bienvenido {nom} {ape}")
 
-hola()
+hola("Maria", "Perez")
+hola("Rodrigo", "Suarez")
+hola("Rosa")
+hola(ape = "Ramirez", nom = "Juan")
