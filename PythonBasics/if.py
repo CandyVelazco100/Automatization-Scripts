2340ed6aa7f9b6fcc203d1116c052ecf10c88e84
Candy Velazco <78711790+CandyVelazco100@users.noreply.github.com>
2024-08-23 11:33:27 -0700

    Ifs

--- PythonBasics/if.py
+++ PythonBasics/if.py
@@ -1,6 +1,8 @@
-edad = 22
+edad = 60
 
-if edad > 17:
+if edad > 54:
+    print("Puedes ver la pelicula con descuento")
+elif edad > 17:
     print("Puede ver la pelicula")
 else:
     print("No puedes entrar")
