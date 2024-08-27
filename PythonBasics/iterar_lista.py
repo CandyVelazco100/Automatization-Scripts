1d00aeaeb72cc3671cd25521313d149b51cc0b9c
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-08-27 11:43:25 -0700

    Listas

--- PythonBasics/iterar_lista.py
+++ PythonBasics/iterar_lista.py
@@ -2,3 +2,10 @@ mascotas = ["Poncho", "Ronald", "Teodoro"]
 
 for indice, mascota in enumerate(mascotas):
     print(indice, mascota)
+
+
+if "Bicho" in mascotas:
+    print(mascotas.index("Bicho"))
+    
+if "Poncho" in mascotas:
+    print(mascotas.index("Poncho"))
