b9bafc2307b00bd3a70972096bad4c39b2fe4c2b
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-08-27 11:28:53 -0700

    listas

---
+++ PythonBasics/listas.py
@@ -0,0 +1,13 @@
+numeros = [1,2,4]
+letras = ["a", "b", "c"]
+palabras = ["hola", "mundo"]
+booleans = [True, False, True]
+matriz = [[0,1],[1,2], [1,0]]
+ceros = [0, 1] * 10
+print(ceros)
+alfanumerico = numeros + letras
+print(alfanumerico)
+rango = list(range(1,11))
+print(rango)
+chars = list("hola mundo")
+print(chars)
