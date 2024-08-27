beb0b52887297996243790bff6e9f5de330d2e59
Candy Velazco <a20490742@itmexicali.edu.mx>
2024-08-27 11:46:48 -0700

    Interaccion listas

--- PythonBasics/iterar_lista.py
+++ PythonBasics/iterar_lista.py
@@ -1,4 +1,7 @@
-mascotas = ["Poncho", "Ronald", "Teodoro"]
+""" Listas en python son dinamicas
+ """
+
+mascotas = ["Poncho", "Ronald", "Teodoro", "Pulga"]
 
 for indice, mascota in enumerate(mascotas):
     print(indice, mascota)
@@ -6,6 +9,16 @@ for indice, mascota in enumerate(mascotas):
 
 if "Bicho" in mascotas:
     print(mascotas.index("Bicho"))
-    
+
 if "Poncho" in mascotas:
     print(mascotas.index("Poncho"))
+
+mascotas.insert(1, 'Melvin')
+print(mascotas)
+mascotas.append('Rosita')
+print(mascotas)
+mascotas.remove("Pulga")
+mascotas.pop()
+""" mascotas.pop(1)"""
+del mascotas[0]
+print(mascotas)
